07f721c50181d9a05b876341448d8cfa4fa54733
Jordan Thivault <jordan.thivault@gmail.com>
2024-06-11 10:51:20 +0200

    react03

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -25,40 +25,53 @@
 //     textShadow: "0px 0px 5px gray",
 //   };
   
-//   function PokemonCard() {
-//     return (
-//       <figure style={card}>
-//         <img
-//           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
-//           alt="charmander"
-//           style={{
-//             width: "100%",
-//             height: "auto",
-//             marginBottom: "0.5rem",
-//             backgroundColor: "white",
-//             borderRadius: "inherit",
-//             boxShadow: "inherit",
-//           }}
-//         />
-//         <figcaption>charmander</figcaption>
-//       </figure>
-//     );
-//   }
+  // function PokemonCard() {
+  //   return (
+  //     <figure style={card}>
+  //       <img
+  //         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
+  //         alt="charmander"
+  //         style={{
+  //           width: "100%",
+  //           height: "auto",
+  //           marginBottom: "0.5rem",
+  //           backgroundColor: "white",
+  //           borderRadius: "inherit",
+  //           boxShadow: "inherit",
+  //         }}
+  //       />
+  //       <figcaption>charmander</figcaption>
+  //     </figure>
+  //   );
+  // }
 
 import styles from './PokemonCard.module.css'
 
 function PokemonCard() {
+  const pokemon = pokemonList[0];
   return (
     <figure className={styles.card}>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
-        alt="squirtle"
-        className={styles.cardImg}
-      />
-      <figcaption>squirtle</figcaption>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} 
+        alt={pokemon.name} 
+        className={styles.cardImg}/>
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 export default PokemonCard;
