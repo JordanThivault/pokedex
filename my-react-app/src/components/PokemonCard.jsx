b0a6f6c89dbd91b9f40f41905c2d031904174881
Jordan Thivault <jordan.thivault@gmail.com>
2024-06-11 10:04:33 +0200

    react2bis

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,12 +1,64 @@
 
 
+// function PokemonCard() {
+//     return (
+//         <figure className="card">
+//             <img 
+//             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
+//             alt="bulbasaur"
+//             className="card-img"
+//             />
+//         <figcaption>Bulbasaur</figcaption>
+//         </figure>
+//     );
+// }
+
+// const card = {
+//     width: "200px",
+//     padding: "1rem",
+//     backgroundColor: "lightsalmon",
+//     borderRadius: "5px",
+//     boxShadow: "0px 0px 5px gray",
+//     color: "white",
+//     fontSize: "1.6rem",
+//     textAlign: "center",
+//     textShadow: "0px 0px 5px gray",
+//   };
+  
+//   function PokemonCard() {
+//     return (
+//       <figure style={card}>
+//         <img
+//           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
+//           alt="charmander"
+//           style={{
+//             width: "100%",
+//             height: "auto",
+//             marginBottom: "0.5rem",
+//             backgroundColor: "white",
+//             borderRadius: "inherit",
+//             boxShadow: "inherit",
+//           }}
+//         />
+//         <figcaption>charmander</figcaption>
+//       </figure>
+//     );
+//   }
+
+import styles from './PokemonCard.module.css'
+
 function PokemonCard() {
-    return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
-        </figure>
-    );
+  return (
+    <figure className={styles.card}>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
+        alt="squirtle"
+        className={styles.cardImg}
+      />
+      <figcaption>squirtle</figcaption>
+    </figure>
+  );
 }
+
 
 export default PokemonCard;
